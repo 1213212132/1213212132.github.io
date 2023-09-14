6e5430238634d793bab7f81bb4cbff3114723739
asdas <95152341+1213212132@users.noreply.github.com>
2023-09-14 14:25:43 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,22 +3,22 @@
 $(function () {
   //알림창
 
-  Swal.fire({
-    title: '환영합니다.',
-    text: '포트폴리오를 봐주셔서 감사합니다.',
-    icon: 'warning',
+  // Swal.fire({
+  //   title: '환영합니다.',
+  //   text: '포트폴리오를 봐주셔서 감사합니다.',
+  //   icon: 'warning',
 
-    showCancelButton: false, // cancel버튼 보이기. 기본은 원래 없음
-    confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
-    confirmButtonText: '확인', // confirm 버튼 텍스트 지정
+  //   showCancelButton: false, // cancel버튼 보이기. 기본은 원래 없음
+  //   confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+  //   confirmButtonText: '확인', // confirm 버튼 텍스트 지정
 
-  }).then(result => {
-    // 만약 Promise리턴을 받으면,
-    if (result.isConfirmed) { // 만약 모달창에서 confirm 버튼을 눌렀다면
+  // }).then(result => {
+  //   // 만약 Promise리턴을 받으면,
+  //   if (result.isConfirmed) { // 만약 모달창에서 confirm 버튼을 눌렀다면
 
-      Swal.fire('감사합니다.', '스크롤을 내리시면 메인화면으로 이동합니다.', 'success');
-    }
-  });
+  //     Swal.fire('감사합니다.', '스크롤을 내리시면 메인화면으로 이동합니다.', 'success');
+  //   }
+  // });
 
 
   // #home nav
